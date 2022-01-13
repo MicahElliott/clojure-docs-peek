b22aa61aa2c1d0ccb92ab7f6ec74af3b0be84b58
Micah Elliott <mde@micahelliott.com>
2022-01-13 13:46:00 -0800

    :cop: Clean up/privatize global names, handle java vars

--- clojure-docs-peek.el
+++ clojure-docs-peek.el
@@ -26,127 +26,136 @@
 (require 'cider)
 (require 'quick-peek)
 
-(defvar cider-inline-docs-opened-p nil
+(defvar cdp--peek-opened-p nil
   "Toggle to support an inline help being open (expanded) or not.")
 
 ;; Get private data
 ;; (cider-interactive-eval "(:private (meta #'crawlingchaos.domain.homeowner.borrower/sfdc-lookup-payments))")
 
+;;;###autoload
 (defun clojure-docs-peek-toggle ()
   "Show a fn or ns docstring's key pieces as inline overlay."
   (interactive)
-  (if cider-inline-docs-opened-p
+  (if cdp--peek-opened-p
       (progn
-	(setq cider-inline-docs-opened-p nil)
+	(setq cdp--peek-opened-p nil)
 	(quick-peek-hide))
+    (setq cdp--ns-p nil)
     ;; If not on a word, let it error
-    (setq my-cidt-ns-p nil)
-    (let* ((info0    (cider-var-info (thing-at-point 'word 'no-properties)))
-	   (info (if (looking-at-fn-p)
-		     info0
-		   (cider-var-info (nrepl-dict-get info0 "ns"))))
-	   (ns      (propertize (nrepl-dict-get info "ns") 'face 'font-lock-type-face))
-           ;; Built-in fns' info are indexed differently from user-defined.
-	   (arglist-str (nrepl-dict-get info "arglists-str"))
-	   (arglist (when arglist-str (propertize arglist-str 'face 'clojure-keyword-face)))
-	   (fname (car (last (split-string (nrepl-dict-get info "file") ":"))))
-	   (name-str (nrepl-dict-get info "name"))
-	   (name    (if (not (string-match "\\." name-str)) ; not an NS
-			(propertize name-str 'face 'font-lock-function-name-face)
-		      ;; (propertize name-str 'face 'font-lock-type-face)
-		      (setq my-cidt-ns-p t)
-		      nil))
-	   (doc     (propertize (nrepl-dict-get info "doc")
-				'face 'font-lock-doc-face))
-	   (stats (if (looking-at-fn-p)
-		      (fn-stats ns name)
-		    (message "ns lookup")
-		    (ns-stats ns)))
-	   (seealso-str (nrepl-dict-get (cider-var-info (thing-at-point 'word 'no-properties)) "see-also"))
-	   (seealso (and seealso-str
-			 (propertize
-			  (concat "\n\nSee-also:\n- "
-				  (mapconcat 'identity
-					     (nrepl-dict-get (cider-var-info (thing-at-point 'word 'no-properties)) "see-also")
-					     "\n- "))
-			  'face font-lock-comment-face)))
-	   (alltext (concat
-		     ;; NS/name heading
-		     (if (string= (cider-current-ns) ns)
-			 name
-		       (if my-cidt-ns-p
-			   ns
-			 (concat ns "/" name)))
-                     "\n"
-		     (if arglist (concat arglist "\n") "")
-		     "\n"
-                     (when doc (replace-regexp-in-string "^  " "" doc))
-		     "\n\n"
-		     stats
-		     ;; Include see-also if universal arg
-		     (when current-prefix-arg seealso))))
-      (if doc
-	  (progn
-	    (message "foo")
-            (setq max-h 30)
-	    (quick-peek-show alltext nil 20)
-            ;; (put-text-property 1 200 'face (cons 'foreground-color "red"))
-	    (add-text-properties 1 300 '(comment t face highlight))
-	    ;; (propertize str-before 'face 'quick-peek-padding-face)
-	    (setq cider-inline-docs-opened-p t))
-	(message "Missing docstring or invalid thing at point")))))
+    (let ((info0    (cider-var-info (thing-at-point 'word 'no-properties))))
+      (if (not (nrepl-dict-get info0 "ns"))
+	  (message "Nothing to do with a java class; try calling: cider-javadoc.")
+	(if (not (nrepl-dict-get info0 "doc"))
+	    (message "Missing docstring or invalid thing at point.")
+	  (let* ((info (if (cdp--looking-at-fn-p)
+			   info0
+			 (cider-var-info (nrepl-dict-get info0 "ns"))))
+		 (ns      (propertize (nrepl-dict-get info "ns") 'face 'font-lock-type-face))
+		 ;; Built-in fns' info are indexed differently from user-defined.
+		 (arglist-str (nrepl-dict-get info "arglists-str"))
+		 (arglist (when arglist-str (propertize arglist-str 'face 'clojure-keyword-face)))
+		 (file-parts (split-string (nrepl-dict-get info "file") ":"))
+		 (is-jar-p   (string-equal "jar" (car file-parts)))
+		 (fname      (car (last file-parts)))
+		 (name-str (nrepl-dict-get info "name"))
+		 (name    (if (not (string-match "\\." name-str)) ; not an NS
+			      (propertize name-str 'face 'font-lock-function-name-face)
+			    ;; (propertize name-str 'face 'font-lock-type-face)
+			    (setq cdp--ns-p t)
+			    nil))
 
-(defun colorize-stat (label num)
+
+		 (doc     (propertize (nrepl-dict-get info "doc")
+				      'face 'font-lock-doc-face))
+		 (stats (if (cdp--looking-at-fn-p)
+			    (cdp--fn-stats ns name)
+			  (message "ns lookup")
+			  (cdp--ns-stats ns is-jar-p)))
+		 (seealso-str (nrepl-dict-get (cider-var-info (thing-at-point 'word 'no-properties)) "see-also"))
+		 (seealso (and seealso-str
+			       (propertize
+				(concat "\n\nSee-also:\n- "
+					(mapconcat 'identity
+						   (nrepl-dict-get (cider-var-info (thing-at-point 'word 'no-properties)) "see-also")
+						   "\n- "))
+				'face font-lock-comment-face)))
+		 (alltext (concat
+			   ;; NS/name heading
+			   (if (string= (cider-current-ns) ns)
+			       name
+			     (if cdp--ns-p
+				 ns
+			       (concat ns "/" name)))
+			   "\n"
+			   (if arglist (concat arglist "\n") "")
+			   "\n"
+			   (when doc (replace-regexp-in-string "^  " "" doc))
+			   "\n\n"
+			   stats
+			   ;; Include see-also if universal arg
+			   (when current-prefix-arg seealso))))
+	    (if doc
+		(progn
+		  (setq max-h 30)
+		  (quick-peek-show alltext nil 50)
+		  ;; (put-text-property 1 200 'face (cons 'foreground-color "red"))
+		  (add-text-properties 1 300 '(comment t face highlight))
+		  ;; (propertize str-before 'face 'quick-peek-padding-face)
+		  (setq cdp--peek-opened-p t))
+	      (message "Not sure how we got here!"))))))))
+
+(defun cdp--colorize-stat (label num &optional face-override)
   "Add face color properties to LABEL and NUM."
   (when num
-    (concat (propertize label 'face 'cider-repl-stdout-face) ":"
+    (concat (propertize label 'face (or face-override 'cider-repl-stdout-face)) ":"
 	    (propertize num   'face 'cider-repl-input-face))))
 
-(defun looking-at-fn-p ()
+(defun cdp--looking-at-fn-p ()
   (let* ((thing (thing-at-point 'word))
 	 (sepi (s-index-of "/" thing))
          (i0 (car (bounds-of-thing-at-point 'word)))
 	 (i2 (cdr (bounds-of-thing-at-point 'word)))
 	 (pt (- (point) i0)))
-    (if (string-match-p "\\." thing)
-	nil
+    (when (not (string-match-p "\\." thing)) ; assume no fully qualified dotted NSs used
       (if (string-match-p "/" thing)
 	  (< sepi pt)))))
 
-(defun fn-stats (ns name)
+(defun cdp--fn-stats (ns name)
   "Gather function stats on NS and NAME."
-  (colorize-stat "refs" (int-to-string (length (cider-sync-request:fn-refs ns name)))))
+  (cdp--colorize-stat "refs" (int-to-string (length (cider-sync-request:fn-refs ns name)))))
 
-(defun ns-stats (ns)
+(defun cdp--ns-stats (ns is-jar-p)
   "Gather namespace stats on NS.
 
 Build a single summary line showing counts of various interesting
 things, such as lines/vars and problems like missing docs and
 flycheck flags."
   (save-window-excursion
-    ;; (message ns)
-    ;; (message name)
     ;; Might need this to avoid read-only prompt
     ;; https://emacs.stackexchange.com/a/19747/11025
     ;; (find-file fname)
     (string-join
      (remq nil (list
-		(colorize-stat "lines"  (int-to-string (count-lines (point-min) (point-max))))
+		(cdp--colorize-stat "lines"  (int-to-string (count-lines (point-min) (point-max))))
 
-		(colorize-stat "vars"   (int-to-string (length (cider-browse-ns--items ns))))
-		(colorize-stat "issues" (let ((issues (split-string
-						       ;; " FlyC" or " FlyC:0|1"
-						       (flycheck-mode-line-status-text) ":")))
-					  (when (= 2 (length issues))
-					    (car (last issues)))))
-		(colorize-stat "nodocs" (int-to-string
-					 (length (remq nil (mapcar (lambda (x) (string-match "Not documented" x))
-								   (cider-browse-ns--items ns) )))))))
+		(cdp--colorize-stat "vars"   (int-to-string (length (cider-browse-ns--items ns))))
+		(when (not is-jar-p)
+		  (cdp--colorize-stat "issues" (let ((issues (split-string
+							      ;; " FlyC" or " FlyC:0|1"
+							      (flycheck-mode-line-status-text) ":")))
+						 (when (= 2 (length issues))
+						   (car (last issues))))
+				      'error))
+		(when (not is-jar-p)
+		  (cdp--colorize-stat "nodocs" (int-to-string
+						(length (remq nil (mapcar (lambda (x) (string-match "Not documented" x))
+									  (cider-browse-ns--items ns) ))))
+				      'error))))
      " — ")))
 
-;; (find-test-file "/home/mde/work/cc/src/clj/crawlingchaos/domain/homeowner/borrower.clj")
+;; (cdp--find-test-file "/home/mde/work/cc/src/clj/crawlingchaos/domain/homeowner/borrower.clj")
 ;; Borrowed from and depends on toggle-test.el
-(defun find-test-file (file)
+(defun cdp--find-test-file (file)
   "Find a test file given source FILE."
   (let ((proj (tgt-proj-for file)))
     (cond (proj
